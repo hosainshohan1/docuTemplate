1e717a8b5003821686f495c7603659bc7c828dce
Md. Shohan Hossain <hossain_shohan1@live.com>
2017-05-20 02:28:56 +0600

    new

--- index.php
+++ index.php
@@ -1,9 +1,29 @@
 <!DOCTYPE html>
-<html>
-<head>
-	<title>docuTemplate</title>
-</head>
-<body>
-<h5>This is a html file</h5>
-</body>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="shortcut icon" type="image/png" href="assets/favicon.ico"/>
+
+    <title>docuTemplate</title>
+
+    <!-- Bootstrap -->
+    <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+
+  </head>
+  <body>
+	<div class="header">
+	    <div class="container text-center">
+			<div class="row">
+				<h3>myProduct V1.01.39</h3>
+				<p>Documentation</p>
+			</div>	
+		</div>
+	</div>
+    <!-- JavaScript -->
+    <script src="assets/js/bootstrap.min.js"></script>
+  </body>
 </html>
